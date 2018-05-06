9d1e24d30ab1123ffd2072742d5442556219c1e3
John Tucker <john.tucker@sharpspring.com>
2018-05-06 12:40:40 -0400

    Setup

---
+++ Assets/Setup.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Setup : MonoBehaviour
+{
+    void Update()
+    {
+        Debug.Log("Update Called");
+    }
+
+	void FixedUpdate()
+	{
+        Debug.Log("FixedUpdate Called");
+	}
+}
