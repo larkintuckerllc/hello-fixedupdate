23d79f396f0fbbd15dcf529f47d95bf1e6f57ef3
John Tucker <john.tucker@sharpspring.com>
2018-05-06 12:55:45 -0400

    Simulate low frame rate

--- Assets/Setup.cs
+++ Assets/Setup.cs
@@ -4,13 +4,21 @@ using UnityEngine;
 
 public class Setup : MonoBehaviour
 {
+    static int TARGET_FRAME_RATE = 10;
+
+    void Awake()
+    {
+        QualitySettings.vSyncCount = 0;
+        Application.targetFrameRate = TARGET_FRAME_RATE;
+    }
+
     void Update()
     {
         Debug.Log("Update Called");
     }
 
-	void FixedUpdate()
-	{
+    void FixedUpdate()
+    {
         Debug.Log("FixedUpdate Called");
-	}
+    }
 }
